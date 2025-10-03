81c5f8c03d82d5b762afb0f1decc4a458ce10ca5
jesusvargasc <estivenvargas0412@gmail.com>
2025-10-03 16:25:26 +0000

    segundo avance

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
+
 int main(){
-    int x;
+    string x;
     bool salida;
 
     cout<<"¿Cuál es el número que desea analizar: "<< endl;
     cin>>x;
 
+    string invertido = x;
+
+    reverse( invertido.begin(),invertido.end())
+
+    if x == invertido{
+        salida = true;
+    }
+    else{
+        salida=false;
+    }
+return 0;
+
+
+
+    
     
 
 
